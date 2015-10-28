1697fd137fefc1ea006bdaa5f96a0aed4648e76d
itsky365 <itsky365@163.com>
2015-10-29 00:25:44 +0800

    FileTest

--- src/com/awifi/toe/FileTest.java
+++ src/com/awifi/toe/FileTest.java
@@ -1,6 +1,7 @@
 package com.awifi.toe;
 
 import java.io.File;
+import java.io.FileOutputStream;
 
 /**
  * Created by zhuxuehuang on 15/10/28.
@@ -8,14 +9,31 @@ import java.io.File;
 
 public class FileTest {
     public static void main(String[] args) {
-        try {
-            File file = new File("a.txt");
-            System.out.println(file.exists());
-            if (!file.exists()) {
+        File file = new File("a.txt");
+        System.out.println(file.getPath());
+        System.out.println(file.getName());
+        System.out.println(file.isFile());
+        System.out.println(file.getAbsolutePath());
+        System.out.println(file.exists());
+
+        if (!file.exists()) {
+            try {
                 file.createNewFile();
+            } catch(Exception e){
+                System.out.println(e);
             }
+        }
+
+        String str = "好好学习 天天向上";
+        try {
+            FileOutputStream fileout = new FileOutputStream(file);
+            fileout.write(str.getBytes());
         } catch (Exception e) {
             System.out.println(e);
+        } finally {
+            System.out.println("完成操作");
         }
+
+
     }
 }
